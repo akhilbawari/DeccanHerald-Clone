19f8d383d0078a325af9391f408f2efc06a24de6
akhilbawari <akhilbawari0804@gmail.com>
2022-07-20 23:11:53 +0530

    added navbar

---
+++ index.js
@@ -0,0 +1,12 @@
+
+// slide nav bar
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+  document.querySelector("#newsletter").addEventListener("click",function(){
+    window.location.href="www.google.com"
+  })
