bfa5d4e56d3e62f947361c304691dbdd1fa5c78c
akhilbawari <akhilbawari0804@gmail.com>
2022-07-21 22:53:08 +0530

    added slide navbar

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 
 // slide nav bar
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
-  document.querySelector("#newsletter").addEventListener("click",function(){
-    window.location.href="www.google.com"
-  })
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+document.querySelector("#newsletter").addEventListener("click",function(){
+  window.location.href="www.google.com"
+})
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+acc[i].addEventListener("click", function() {
+  this.classList.toggle("active");
+  var panel = this.nextElementSibling;
+  if (panel.style.maxHeight) {
+    panel.style.maxHeight = null;
+  } else {
+    panel.style.maxHeight = panel.scrollHeight + "px";
+  } 
+});
+}
