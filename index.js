cd2f2b25af4fd5b4523bc2a000bc6bd07cdb9c87
akhilbawari <akhilbawari0804@gmail.com>
2022-07-23 01:28:12 +0530

    added footer

--- index.js
+++ index.js
@@ -24,3 +24,31 @@ acc[i].addEventListener("click", function() {
   } 
 });
 }
+
+let imagelink=[
+  "https://www.deccanherald.com/sites/dh/files/styles/article_detail/public/articleimages/2022/07/22/gold-reuters-1129075-1658489368.jpg?itok=vhaVGBFe",
+  "https://www.deccanherald.com/sites/dh/files/styles/gallery_thumbnails/public/gallery_images/2022/07/22/National%20FIlm%20Awards.jpg",
+  "https://www.deccanherald.com/sites/dh/files/styles/article_detail/public/articleimages/2022/07/22/belagavi-dh-1129081-1658490159.jpg?itok=sVLTUnZl",
+  "https://www.deccanherald.com/sites/dh/files/styles/article_detail/public/articleimages/2022/07/22/dominos-reuters-1129090-1658492242.jpg?itok=_bcB1DSE",
+
+]
+let leftBtn=document.getElementById("left-btn");
+let rightBtn=document.getElementById("right-btn");
+let crauser=document.getElementById("crauser")
+let imagestate=0;
+rightBtn.addEventListener("click",function(){
+  imagestate++
+  if(imagestate===imagelink.length){
+    imagestate=0;
+  }
+  crauser.src=imagelink[imagestate];
+
+})
+leftBtn.addEventListener("click",function(){
+  imagestate--
+  if(imagestate<0){
+    imagestate=imagelink.length-1;
+  }
+  crauser.src=imagelink[imagestate];
+
+})
